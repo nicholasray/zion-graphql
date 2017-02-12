5985e760cc23ff46d5eac5366a2ab2f657950713
Nicholas Ray <nrayemail@gmail.com>
2017-02-11 19:14:58 -0700

    travis

--- db/migrateSeeds.js
+++ db/migrateSeeds.js
@@ -1,7 +1,10 @@
 var config      = require('../knexfile.js');
 console.log(config);
 var knex        = require('knex')(config);
-console.log(knex);
+
+knex.raw('select 1+1 as result').then(function () {
+  // there is a valid connection in the pool
+});
 
 knex.migrate.rollback()
 .then(() => {
