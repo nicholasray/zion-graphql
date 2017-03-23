42ec11bbcef0d9764bc90f0b9ab2317e581d71bd
Nicholas Ray <nrayemail@gmail.com>
2017-03-23 01:08:03 -0600

    Change seeds

--- db/seeds/trips.js
+++ db/seeds/trips.js
@@ -167,7 +167,7 @@ function createTrip(areaId, knex) {
     description: getMarkdown(),
     directions: faker.lorem.paragraph(),
     permit: faker.lorem.paragraph(),
-    permit_path: faker.helpers.randomize(['/zion', '/canyonlands', null]),
+    permit_path: faker.helpers.randomize(['/zion', '/canyonlands?area=maze', '/canyonlands?area=island-in-the-sky', '/canyonlands?area=needles', null]),
     lat: faker.random.number({
       min: 38,
       max: 41,
