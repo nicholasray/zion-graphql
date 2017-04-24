ebb61cb20920ef9a92e8ebfbe9fb8a01cd1415a8
Nicholas Ray <nrayemail@gmail.com>
2017-04-24 00:26:57 -0600

    fix bug

--- src/campsite/index.js
+++ src/campsite/index.js
@@ -1,7 +1,9 @@
 const Dao = require('./dao')
 
-function init(db, campsiteImageDao, config) {
-  const dao = new Dao(db, campsiteImageDao);
+function init(db, daos, config) {
+  const dao = new Dao(db, daos);
+
+  initEndpoints(dao, config);
   initSchema(config);
 
   return {
@@ -20,9 +22,34 @@ function initSchema(config) {
       createdAt: String!
       updatedAt: String!
     }
+
+    input CampsiteInput {
+      name: String
+      lat: Float
+      lng: Float
+    }
+
+    type CampsiteResponse {
+      node: Campsite
+      errors: [ResponseError!]!
+    }
   `;
 
-  config.addSchemaTypesAndEndpoints(types, '');
+  const mutationEndpoints = `
+    updateCampsite(id: ID!, input: CampsiteInput): CampsiteResponse
+  `
+
+  config.addSchemaTypesAndEndpoints(types, '', mutationEndpoints);
+}
+
+function initEndpoints(dao, config) {
+  const endpoints = {
+    updateCampsite: ({id, input}) => {
+      return dao.update(id, input);
+    }
+  };
+
+  config.addEndpoints(endpoints);
 }
 
 module.exports = {
