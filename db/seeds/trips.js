30d0b69078ba854afe2cb686b7292a33f245d0c4
Nicholas Ray <nrayemail@gmail.com>
2017-05-20 13:21:45 -0600

    Add seed files

--- db/seeds/trips.js
+++ db/seeds/trips.js
@@ -278,48 +278,10 @@ function slugify(text) {
 }
 
 function getFilenames() {
-  const l = [
-    "DSCF1260.jpg",
-    "DSCF1261.jpg",
-    "DSCF1263.jpg",
-    "DSCF1265.jpg",
-    "DSCF1266.jpg",
-    "DSCF1270.jpg",
-    "DSCF1271.jpg",
-    "DSCF1272.jpg",
-    "DSCF1273.jpg",
-    "DSCF1274.jpg",
-    "DSCF1275.jpg",
-    "DSCF1276.jpg",
-    "DSCF1277.jpg",
-    "DSCF1278.jpg",
-    "DSCF1280.jpg",
-    "DSCF1281.jpg",
-    "DSCF1282.jpg",
-    "DSCF1284.jpg",
-    "DSCF1285.jpg",
-    "DSCF1286.jpg",
-    "DSCF1287.jpg",
-    "DSCF1288.jpg",
-    "DSCF1289.jpg",
-    "DSCF1290.jpg",
-    "DSCF1291.jpg",
-    "DSCF1293.jpg",
-    "DSCF1294.jpg",
-    "DSCF1295.jpg"
-  ]
+  const random = faker.random.number({
+    min: 1,
+    max: 40
+  });
 
-  shuffle(l);
-
-  return l;
-}
-
-function shuffle(a) {
-    var j, x, i;
-    for (i = a.length; i; i--) {
-        j = Math.floor(Math.random() * i);
-        x = a[i - 1];
-        a[i - 1] = a[j];
-        a[j] = x;
-    }
+  return [random + ".jpg"];
 }
