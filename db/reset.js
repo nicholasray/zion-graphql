4fa50bb9e9fdb1ba3ea68af0bab43679801f4eb2
Nicholas Ray <nrayemail@gmail.com>
2017-05-25 21:39:41 -0600

    Change order

--- db/reset.js
+++ db/reset.js
@@ -1,15 +1,14 @@
 module.exports = function(knex) {
   return Promise.all([
-      knex.schema.dropTableIfExists('trip_images'),
-      knex.schema.dropTableIfExists('campsite_images'),
-      knex.schema.dropTableIfExists('itinerary_plans'),
-      knex.schema.dropTableIfExists('itineraries'),
-      knex.schema.dropTableIfExists('trip_campsites'),
-      knex.schema.dropTableIfExists('campsites'),
-      knex.schema.dropTableIfExists('trip_reports'),
-      knex.schema.dropTableIfExists('images'),
-      knex.schema.dropTableIfExists('trips'),
-      knex.schema.dropTableIfExists('areas'),
-      knex.schema.dropTableIfExists('users')
+      knex.raw('DROP TABLE IF EXISTS trip_images CASCADE'),
+      knex.raw('DROP TABLE IF EXISTS campsite_images CASCADE'),
+      knex.raw('DROP TABLE IF EXISTS itinerary_plans CASCADE'),
+      knex.raw('DROP TABLE IF EXISTS trip_campsites CASCADE'),
+      knex.raw('DROP TABLE IF EXISTS campsites CASCADE'),
+      knex.raw('DROP TABLE IF EXISTS trip_reports CASCADE'),
+      knex.raw('DROP TABLE IF EXISTS images CASCADE'),
+      knex.raw('DROP TABLE IF EXISTS trips CASCADE'),
+      knex.raw('DROP TABLE IF EXISTS areas CASCADE'),
+      knex.raw('DROP TABLE IF EXISTS users CASCADE'),
   ]);
 }
