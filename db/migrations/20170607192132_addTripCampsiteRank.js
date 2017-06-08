7c19fb17132e0c3ec1fde30a615ce8ef20980f8e
Nicholas Ray <nrayemail@gmail.com>
2017-06-07 21:31:47 -0600

    revise migration

--- db/migrations/20170607192132_addTripCampsiteRank.js
+++ db/migrations/20170607192132_addTripCampsiteRank.js
@@ -1,13 +1,15 @@
 exports.up = function(knex, Promise) {
   return knex.schema.table('trip_campsites', function(table) {
     table.integer('rank')
-      .notNullable();
+      .notNullable()
+      .defaultTo(0);
   });
 };
 
 exports.down = function(knex, Promise) {
   return knex.schema.table('trip_campsites', function(table) {
     table.text('rank')
-      .notNullable();
+      .notNullable()
+      .defaultTo(0);
   });
 };
